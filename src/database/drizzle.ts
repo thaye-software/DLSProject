0b37d79c3f9935e27ab45a796dbeb2a71bda6a07
Chris <chye0001@stud.kea.dk>
2025-11-03 12:25:59 +0100

    replaced NODE_ENV with APP_ENV as NODE_ENV forced env to be "development"

--- src/database/drizzle.ts
+++ src/database/drizzle.ts
@@ -2,12 +2,14 @@ import '../../envConfig.ts';
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-const connectionString = process.env.NODE_ENV == 'production' 
+const connectionString = process.env.APP_ENV == 'production' 
   ? process.env.DATABASE_URL_PROD
-  : process.env.NODE_ENV == 'development'
+  : process.env.APP_ENV == 'development'
   ? process.env.DATABASE_URL_DEV
   : process.env.DATABASE_URL_LOCAL; 
 
+  console.log("Database connection string:", connectionString);
+  console.log("Database connection string:", process.env.APP_ENV);
 if (!connectionString) {
   throw new Error("DATABASE_URL is not set in environment variables");
 }
