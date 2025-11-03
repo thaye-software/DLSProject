2c00f8fc869f202a90e3306ba08193ca1c79ecb7
madsfjeldberg <mafj0001@stud.kea.dk>
2025-11-03 10:47:41 +0100

    added migration for order_addresses table

--- frontend/drizzle.config.ts
+++ frontend/drizzle.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "drizzle-kit";
 import './envConfig.ts';
 
 export default defineConfig({
-  out: "./drizzle/migrations",
+  out: "./src/database/migrations",
   dialect: "postgresql",
-  schema: "./drizzle/schema.ts",
+  schema: "./src/database/schema.ts",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
