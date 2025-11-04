b50700b9d5100a9c80bf49be49e1caf17286ae74
Chris <chye0001@stud.kea.dk>
2025-11-04 16:44:09 +0100

    added workflow for test

--- __tests__/integration/page.test.tsx
+++ __tests__/integration/page.test.tsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
-import Page from '../src/app/page.tsx'
+import Page from '../../src/app/page.tsx'
 
 describe('Home page content', () => {
   it('displays the homepage text', () => {
