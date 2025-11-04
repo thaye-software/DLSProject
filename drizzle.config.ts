0caf6b817d6c74520df2272d4bc558b26c07c577
madsfjeldberg <mafj0001@stud.kea.dk>
2025-11-04 11:36:22 +0100

    updated migrations and schema layout

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "drizzle-kit";
-import './envConfig.js';
+import './envConfig.ts';
 
 const connectionString = process.env.APP_ENV == 'prod' 
   ? process.env.DATABASE_URL_PROD
