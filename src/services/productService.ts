8370aa32dd75e4b58bb2533d3657c87b28db1144
Chris <chye0001@stud.kea.dk>
2025-11-08 22:20:22 +0100

    created function in products service to get all products on brandname/slug

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,6 +1,11 @@
+import "server-only";
+
 import { db } from "@/database/drizzle";
-import { products } from "@/database/migrations/schema";
+import { products, brands } from "@/database/schema.ts";
 import { NewProductModel } from "@/database/types";
+
+import { Products } from "../app/watches/type";
+import { eq } from "drizzle-orm";
 
 export const productService = {
   
@@ -16,7 +21,7 @@ export const productService = {
 
   async getAllProducts() {
     try {
-      const allProducts = await db.query.products.findMany({
+      const allProducts: Products[] = await db.query.products.findMany({
         with: {
           watch: {
             with: {
@@ -30,10 +35,38 @@ export const productService = {
           productImages: true,
         },
       });
-      return { success: true, data: allProducts };
+
+      return allProducts;
+
     } catch (error) {
       console.error("Error fetching products:", error);
-      return { success: false, error: "Failed to fetch products" };
+      throw new Error("Failed to fetch products from database");
     }
   },
+
+  //TODO optimise this funciton
+  async getAllProductsByBrandName(brandSlug: string): Promise<Products[]> {
+    try {
+      const brand = await db.query.brands.findFirst({
+        where: eq(brands.slug, brandSlug),
+      });
+
+      if (!brand) {
+        return [];
+      }
+
+      const allProducts: Products[] = await this.getAllProducts();
+
+      const filteredProducts = allProducts.filter(
+        (product) => product.watch?.brand.id === brand.id
+      );
+
+      return filteredProducts;
+      
+    } catch (error) {
+      console.error("Error fetching products by brand slug:", error);
+      throw new Error("Failed to fetch products from database");
+    }
+  }
+
 };
