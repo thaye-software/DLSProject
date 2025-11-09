9d703ddc9fe03164adf9f28deca59d88059b919e
Chris <chye0001@stud.kea.dk>
2025-11-09 15:57:40 +0100

    added function for getting watch by id

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -4,7 +4,7 @@ import { db } from "@/database/drizzle";
 import { products, brands } from "@/database/schema.ts";
 import { NewProductModel } from "@/database/types";
 
-import { Products } from "../app/watches/type";
+import { Product } from "../app/watches/type";
 import { eq } from "drizzle-orm";
 
 export const productService = {
@@ -21,7 +21,7 @@ export const productService = {
 
   async getAllProducts() {
     try {
-      const allProducts: Products[] = await db.query.products.findMany({
+      const allProducts: Product[] = await db.query.products.findMany({
         with: {
           watch: {
             with: {
@@ -45,7 +45,7 @@ export const productService = {
   },
 
   //TODO optimise this funciton
-  async getAllProductsByBrandName(brandSlug: string): Promise<Products[]> {
+  async getAllProductsByBrandName(brandSlug: string): Promise<Product[]> {
     try {
       const brand = await db.query.brands.findFirst({
         where: eq(brands.slug, brandSlug),
@@ -55,7 +55,7 @@ export const productService = {
         return [];
       }
 
-      const allProducts: Products[] = await this.getAllProducts();
+      const allProducts: Product[] = await this.getAllProducts();
 
       const filteredProducts = allProducts.filter(
         (product) => product.watch?.brand.id === brand.id
@@ -67,6 +67,38 @@ export const productService = {
       console.error("Error fetching products by brand slug:", error);
       throw new Error("Failed to fetch products from database");
     }
+  },
+
+  async getProductById(watchId: string | number): Promise<Product | null> {
+    try {
+      const watch = await db.query.products.findFirst({
+        where: eq(products.id, Number(watchId)),
+        
+        with: {
+          watch: {
+            with: {
+              brand: {
+                with: {
+                  productSafetyInfo: true,
+                },
+              },
+            },
+          },
+          productImages: true,
+        },
+      })
+
+      if(watch === undefined || watch === null) {
+        return null
+      }
+
+      return watch;
+
+    } catch(error) {
+      console.error(error);
+      throw error;
+    }
+
   }
 
 };
