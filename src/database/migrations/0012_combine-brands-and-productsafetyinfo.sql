f0875ff499ab554245610d174275933a32090ced
madsfjeldberg <mafj0001@stud.kea.dk>
2025-11-10 10:04:31 +0100

    add db migration

---
+++ src/database/migrations/0012_combine-brands-and-productsafetyinfo.sql
@@ -0,0 +1,16 @@
+ALTER TABLE "product_safety_info" DISABLE ROW LEVEL SECURITY;--> statement-breakpoint
+DROP TABLE "product_safety_info" CASCADE;--> statement-breakpoint
+ALTER TABLE "brands" DROP CONSTRAINT "brands_product_safety_info_id_product_safety_info_id_fk";
+--> statement-breakpoint
+DROP INDEX "idx_brands_slug";--> statement-breakpoint
+ALTER TABLE "brands" ADD COLUMN "country" varchar(255) DEFAULT '' NOT NULL;--> statement-breakpoint
+ALTER TABLE "brands" ADD COLUMN "address_line_1" varchar(255);--> statement-breakpoint
+ALTER TABLE "brands" ADD COLUMN "address_line_2" varchar(255);--> statement-breakpoint
+ALTER TABLE "brands" ADD COLUMN "zip_code" varchar(50);--> statement-breakpoint
+ALTER TABLE "brands" ADD COLUMN "city" varchar(255);--> statement-breakpoint
+ALTER TABLE "brands" ADD COLUMN "state_province" varchar(255);--> statement-breakpoint
+ALTER TABLE "brands" ADD COLUMN "phone_number" varchar(50);--> statement-breakpoint
+ALTER TABLE "brands" ADD COLUMN "email" varchar(255);--> statement-breakpoint
+ALTER TABLE "brands" ADD COLUMN "website" varchar(255);--> statement-breakpoint
+ALTER TABLE "brands" DROP COLUMN "slug";--> statement-breakpoint
+ALTER TABLE "brands" DROP COLUMN "product_safety_info_id";
