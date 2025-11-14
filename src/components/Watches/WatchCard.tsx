f5cf253095d3a3a858523eaaa0a7a1c905ae5a0a
madsfjeldberg <mafj0001@stud.kea.dk>
2025-11-14 12:27:36 +0100

    updated watchcard and grid

--- src/components/Watches/WatchCard.tsx
+++ src/components/Watches/WatchCard.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import { ShoppingCart } from "lucide-react";
+import { Heart, ShoppingCart } from "lucide-react";
 
 import { Product } from "@/app/watches/type";
 import { convertPrice } from "@/services/currencyService";
@@ -20,63 +20,66 @@ export async function WatchCard({ product, countryCode }: { product: Product, co
 
   return (
 
-            <Card className="group overflow-hidden transition-all duration-300 hover:shadow-2xl border-0 p-0">
+            <Card className="group bg-transparent overflow-hidden transition-all duration-300 border-0 shadow-none p-0">
                 <Link href={`/watches/view/${product.watch.slug}`}>
                     <CardHeader className="p-0">
-                        <div className="relative overflow-hidden aspect-4/5 bg-muted">
+                        <div className="relative overflow-hidden aspect-square rounded-2xl">
                             <Image
                                 src={product.productImages[0]?.imageUrl || "/sadly-no-image.png"}
                                 alt={`${product.watch.brand.name} ${product.name}`}
-                                className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-105"
+                                className="w-full h-full object-cover rounded-2xl transition-transform duration-700 group-hover:scale-105"
                                 fill
                                 unoptimized // REMOVE THIS IN PRODUCTION
                             />
 
-                            <div className="absolute top-4 right-4 flex gap-2">
-                                {product.stock > 0 ? (
-                                    <Badge variant="secondary" className="bg-background/95 backdrop-blur-sm shadow-lg">
-                                        In Stock
-                                    </Badge>
-                                ): 
-                                    <Badge variant="secondary" className="bg-background/95 backdrop-blur-sm shadow-lg">
+            <div className="absolute top-4 right-4 flex gap-2 group">
+              <Button size="icon" variant="secondary" className="opacity-0 group-hover:opacity-100 cursor-pointer bg-background/95 shadow-lg">
+                <Heart />
+              </Button>
+              {/* {product.stock > 0 ? (
+                  <Badge variant="secondary" className="bg-background/95  shadow-lg">
+                      In Stock
+                  </Badge>
+              ): 
+                                    <Badge variant="secondary" className="bg-background/95 shadow-lg">
                                         Sold out
-                                    </Badge>}
+                                    </Badge>} */}
                             </div>
                         </div>
                     </CardHeader>
                 </Link> 
             
-                <CardContent className="p-6 flex flex-col justify-between h-full">
-                    <div className="space-y-2">
+                <CardContent className="pb-2 gap-2 flex flex-col justify-between h-full px-3">
+                    <div className="">
                         {/* Brand + Model */}
                             <p className="text-xs text-muted-foreground font-semibold tracking-[0.2em] uppercase line-clamp-1">
-                        {product.watch.brand.name} - {product.watch.model}
+                        {product.watch.brand.name}
                         </p>
 
                         {/* Watch Name */}
-                            <h3 className="text-2xl font-semibold mt-1 line-clamp-1">
+                            <h3 className="text-xl font-bold line-clamp-1">
                         {product.name}
                         </h3>
 
-                        {/* Description */}
-                        <p className="text-sm text-muted-foreground leading-relaxed line-clamp-2 min-h-12">
-                            {product.description}
+                        {/* Reference */}
+                        <p className="text-sm text-muted-foreground leading-relaxed">
+                            ref. {product.watch.reference}
                         </p>
                     </div>
 
                     {/* Price + Button */}
-                    <div className="flex items-center justify-between gap-4 pt-2 mt-auto">
-                        <p className="text-3xl font-bold">
+                    <div className="flex items-center justify-between mt-auto">
+                        <p className="text-xl font-bold">
                             {formattedPrice}
                         </p>
 
-                        <div className="overflow-hidden">
+                        {/* <div className="overflow-hidden">
                             <AddToCartButton 
                                 product={product}
                                 className="hover:cursor-pointer opacity-0 group-hover:opacity-100 -translate-x-4 group-hover:translate-x-0 transition-all duration-700 ease-out whitespace-nowrap"
 
                             />
-                        </div>
+                        </div> */}
                     </div>
                 </CardContent>
             </Card>
--- src/components/Watches/WatchesGrid.tsx
+++ src/components/Watches/WatchesGrid.tsx
@@ -4,7 +4,7 @@ import { Product } from "../../app/watches/type";
 
 export async function WatchesGrid({watches, countryCode}: {watches: Product[], countryCode: string}) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6 w-full">
       {watches.map((watch) => (
         <WatchCard key={watch.id} product={watch} countryCode={countryCode} />
       ))}
