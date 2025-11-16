12a06c15f3d05df62e9ded9638d78e54cece6ca6
madsfjeldberg <mafj0001@stud.kea.dk>
2025-11-16 15:44:48 +0100

    some cool motion shit

--- src/components/Watches/WatchCard.tsx
+++ src/components/Watches/WatchCard.tsx
@@ -1,34 +1,48 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
 import { Product } from "@/app/watches/type";
-import { convertPrice } from "@/services/currencyService";
 import FavoriteButton from "./FavoriteButton";
+import { motion } from "framer-motion";
 
-
-export async function WatchCard({ product, countryCode }: { product: Product, countryCode: string }) {
-
-  const formattedPrice = await convertPrice(product.priceDkk, countryCode.toLowerCase());
-
+export function WatchCard({
+  product,
+  countryCode,
+  formattedPrice,
+  index,
+}: {
+  product: Product;
+  countryCode: string;
+  formattedPrice: Promise<string>;
+  index?: number;
+}) {
   return (
+    <motion.div
+      initial={{ opacity: 0, y: -20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5, delay: (index ?? 0) * 0.07 }}
+    >
+      <Card className="group bg-transparent overflow-hidden transition-all duration-300 border-0 shadow-none p-0 gap-1">
+        <Link href={`/watches/view/${product.watch.slug}`}>
+          <CardHeader className="p-0">
+            <div className="relative overflow-hidden aspect-square rounded-2xl">
+              <Image
+                src={
+                  product.productImages[0]?.imageUrl || "/sadly-no-image.png"
+                }
+                alt={`${product.watch.brand.name} ${product.name}`}
+                className="w-full h-full object-cover rounded-2xl transition-transform duration-700 group-hover:scale-105"
+                fill
+                unoptimized // REMOVE THIS IN PRODUCTION
+              />
 
-            <Card className="group bg-transparent overflow-hidden transition-all duration-300 border-0 shadow-none p-0 gap-1">
-                <Link href={`/watches/view/${product.watch.slug}`}>
-                    <CardHeader className="p-0">
-                        <div className="relative overflow-hidden aspect-square rounded-2xl">
-                            <Image
-                                src={product.productImages[0]?.imageUrl || "/sadly-no-image.png"}
-                                alt={`${product.watch.brand.name} ${product.name}`}
-                                className="w-full h-full object-cover rounded-2xl transition-transform duration-700 group-hover:scale-105"
-                                fill
-                                unoptimized // REMOVE THIS IN PRODUCTION
-                            />
-
-            <div className="absolute top-4 right-4 flex gap-2 group">
-              <FavoriteButton productId={product.id} />
-              {/* {product.stock > 0 ? (
+              <div className="absolute top-4 right-4 flex gap-2 group">
+                <FavoriteButton productId={product.id} />
+                {/* {product.stock > 0 ? (
                   <Badge variant="secondary" className="bg-background/95  shadow-lg">
                       In Stock
                   </Badge>
@@ -36,45 +50,41 @@ export async function WatchCard({ product, countryCode }: { product: Product, co
                                     <Badge variant="secondary" className="bg-background/95 shadow-lg">
                                         Sold out
                                     </Badge>} */}
-                            </div>
-                        </div>
-                    </CardHeader>
-                </Link> 
-            
-                <CardContent className="gap-2 flex flex-col justify-between h-full px-3">
-                    <div className="">
-                        {/* Brand + Model */}
-                            <p className="text-xs text-muted-foreground font-semibold tracking-[0.2em] uppercase line-clamp-1">
-                        {product.watch.brand.name}
-                        </p>
+              </div>
+            </div>
+          </CardHeader>
+        </Link>
 
-                        {/* Watch Name */}
-                            <h3 className="text-xl font-bold line-clamp-1">
-                        {product.name}
-                        </h3>
+        <CardContent className="gap-2 flex flex-col justify-between h-full px-3">
+          <div className="">
+            {/* Brand + Model */}
+            <p className="text-xs text-muted-foreground font-semibold tracking-[0.2em] uppercase line-clamp-1">
+              {product.watch.brand.name}
+            </p>
 
-                        {/* Reference */}
-                        <p className="text-sm text-muted-foreground leading-relaxed">
-                            ref. {product.watch.reference}
-                        </p>
-                    </div>
+            {/* Watch Name */}
+            <h3 className="text-xl font-bold line-clamp-1">{product.name}</h3>
 
-                    {/* Price + Button */}
-                    <div className="flex items-center justify-between mt-auto">
-                        <p className="text-xl font-bold">
-                            {formattedPrice}
-                        </p>
+            {/* Reference */}
+            <p className="text-sm text-muted-foreground leading-relaxed">
+              ref. {product.watch.reference}
+            </p>
+          </div>
 
-                        {/* <div className="overflow-hidden">
+          {/* Price + Button */}
+          <div className="flex items-center justify-between mt-auto">
+            <p className="text-xl font-bold">{formattedPrice}</p>
+
+            {/* <div className="overflow-hidden">
                             <AddToCartButton 
                                 product={product}
                                 className="hover:cursor-pointer opacity-0 group-hover:opacity-100 -translate-x-4 group-hover:translate-x-0 transition-all duration-700 ease-out whitespace-nowrap"
 
                             />
                         </div> */}
-                    </div>
-                </CardContent>
-            </Card>
-           
+          </div>
+        </CardContent>
+      </Card>
+    </motion.div>
   );
-};
+}
--- src/components/Watches/WatchesGrid.tsx
+++ src/components/Watches/WatchesGrid.tsx
@@ -1,13 +1,32 @@
 import { WatchCard } from "@/components/Watches/WatchCard";
 
 import { Product } from "../../app/watches/type";
+import { convertPrice } from "@/services/currencyService";
 
-export async function WatchesGrid({ watches, countryCode }: { watches: Product[], countryCode: string }) {
+export async function WatchesGrid({
+  watches,
+  countryCode,
+}: {
+  watches: Product[];
+  countryCode: string;
+}) {
+  async function getFormattedPrice(
+    productPriceDkk: number,
+    countryCode: string
+  ) {
+    return await convertPrice(productPriceDkk, countryCode.toLowerCase());
+  }
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6 w-full">
-      {watches.map((watch) => (
-        <WatchCard key={watch.id} product={watch} countryCode={countryCode} />
+      {watches.map((watch, i) => (
+        <WatchCard
+          key={watch.id}
+          product={watch}
+          countryCode={countryCode}
+          formattedPrice={getFormattedPrice(watch.priceDkk, countryCode)}
+          index={i}
+        />
       ))}
     </div>
   );
