5a8149b91a5f47be202a62e0451b9a6e66aa414a
Chris <chye0001@stud.kea.dk>
2025-11-29 14:10:12 +0100

    created action to handling changing username and email

---
+++ src/app/settings/actions.ts
@@ -0,0 +1,52 @@
+"use server"
+
+import { changeUsername, getUserById, initiateEmailChange } from "@/services/userService";
+
+
+
+
+export async function changeUsernameAndEmail(formData: any) {
+   
+    try {
+        const customer = transformFormData(formData);
+        const foundUser = await getUserById(customer.id);
+        if(!foundUser) {
+            return {userNotFound: true}
+        }
+        
+        
+        let isUsernameChanged = false;
+        if(customer.newUsername !== foundUser?.username) {
+            const changedInfo = await changeUsername(foundUser.id, customer.newUsername);
+            if(changedInfo) {
+                isUsernameChanged = true;
+            }
+        }
+        
+        
+        let isEmailedChangeInitiated = false;
+        if(customer.newEmail !== foundUser?.email) {
+            const initiated = await initiateEmailChange(foundUser.id, customer.newEmail);
+            if(initiated) {
+                isEmailedChangeInitiated = true;
+            }
+        }
+        
+        return {isUsernameChanged, isEmailedChangeInitiated};
+    
+    } catch (error) {
+        console.error("unexpted error orcoured changing ",error);
+        throw error;
+    }   
+}
+
+function transformFormData(formData: any) {
+    const formDataObject = {
+        newUsername: formData.newUsername as string,
+        newEmail: formData.newEmail as string,
+        id: formData.customerId as string
+    }
+
+    return formDataObject;
+}
+
