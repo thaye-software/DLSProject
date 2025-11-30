2435fad4796115550c5b57ac53d63357c5cbc4e3
madsfjeldberg <mafj0001@stud.kea.dk>
2025-11-30 12:32:33 +0100

    added missing files whoops

---
+++ src/components/Navbar/BrandNavItem.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import {
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { BrandModel } from "@/database/types";
+import { getAllBrands } from "@/services/brandService";
+
+export default function BrandNavItem() {
+  const [brands, setBrands] = useState<BrandModel[]>([]);
+
+  useEffect(() => {
+    async function fetchBrands() {
+      const fetchedBrands = await getAllBrands();
+      setBrands(fetchedBrands);
+    }
+    fetchBrands();
+  }, []);
+
+  return (
+    <NavigationMenuItem className="hidden md:block">
+      <NavigationMenuTrigger className="font-bold">
+        Brands
+      </NavigationMenuTrigger>
+      <NavigationMenuContent>
+        <ul className="grid w-[200px] gap-2">
+          {brands.map((brand) => (
+            <li key={brand.id}>
+              <NavigationMenuLink asChild>
+                <Link
+                  href={`/watches/${brand.slug}`}
+                >
+                  {brand.name}
+                </Link>
+              </NavigationMenuLink>
+            </li>
+          ))}
+        </ul>
+      </NavigationMenuContent>
+    </NavigationMenuItem>
+  );
+}
