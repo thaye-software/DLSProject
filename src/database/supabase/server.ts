494918b5534e988efbb2dba3fb8ace68d3f29a44
Chris <chye0001@stud.kea.dk>
2025-11-30 18:48:52 +0100

    made server supabse client use the service role key instead of public key used in client

--- src/database/supabase/server.ts
+++ src/database/supabase/server.ts
@@ -3,10 +3,10 @@ import { cookies } from 'next/headers'
 
 export async function createClient() {
   const cookieStore = await cookies()
-
+  const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    supabaseServiceRoleKey,
     {
       cookies: {
         getAll() {
