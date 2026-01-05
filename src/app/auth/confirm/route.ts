099d5690d301aa370e6a9ff65c472295d44c1b15
Chris <chye0001@stud.kea.dk>
2026-01-05 17:33:18 +0100

    updated redirect page

--- src/app/auth/confirm/route.ts
+++ src/app/auth/confirm/route.ts
@@ -3,6 +3,9 @@ import { type NextRequest } from 'next/server'
 
 import { createClient } from '@/lib/supabase/server'
 import { redirect } from 'next/navigation'
+import { eq } from 'drizzle-orm'
+import { users } from '@/database/schema'
+import { db } from '@/database/drizzle'
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
@@ -31,6 +34,18 @@ export async function GET(request: NextRequest) {
       redirect(next)
     }
   }
+
+  if (type === 'email_change') {
+    const supabase = await createClient()
+    const { data: { user } } = await supabase.auth.getUser()
+      
+    if (user) {
+      await db
+        .update(users)
+        .set({ email: user.email })
+        .where(eq(users.id, user.id))
+      }
+    }
 
   // redirect the user to an error page with some instructions
   redirect('/error')
