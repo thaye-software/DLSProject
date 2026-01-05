d3fb5d6bf0f0273ef4234b68c1811ae57b912730
Chris <chye0001@stud.kea.dk>
2026-01-05 17:33:52 +0100

    moved email update for custome users table to redirect endpoint for confirming

--- src/app/confirm-email-change/page.tsx
+++ src/app/confirm-email-change/page.tsx
@@ -1,16 +1,23 @@
-export default function ConfirmChangeEmailPage({
+export default async function ConfirmChangeEmailPage({
   searchParams,
 }: {
   searchParams: { message?: string };
 }) {
-    const message = searchParams.message;
+	const message = (await searchParams).message;
 
-    return(
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1 className="text-3xl font-bold mb-4">Email Change Confirmed</h1>
-            <p className="text-lg">Your email address has been successfully updated.</p>
-
-            <p>{message}</p>
-        </div>
-    )
+	return(
+		<div>
+			{message ? (
+				<div className="flex flex-col items-center justify-center mt-26">
+					<h1 className="text-3xl font-bold mb-4">Email Change Initiated</h1>
+					<p className="text-lg">{message}.</p>
+				</div>
+			) : (
+				<div className="flex flex-col items-center justify-center mt-26">
+					<h1 className="text-3xl font-bold mb-4">Email Change Confirmed</h1>
+					<p className="text-lg">Your email has been successfully changed.</p>
+				</div>
+			)}
+		</div>
+	)
 }
